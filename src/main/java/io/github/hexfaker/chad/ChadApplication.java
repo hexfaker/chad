021c268d814ae3478c27d567ba1d96e27636ba9a
equeezys <tvl95@bk.ru>
2018-04-18 20:40:50 +0300

    initial commit

---
+++ src/main/java/io/github/hexfaker/chad/ChadApplication.java
@@ -0,0 +1,12 @@
+package io.github.hexfaker.chad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChadApplication.class, args);
+	}
+}
